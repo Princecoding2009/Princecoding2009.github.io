758e4c41d8e3adb350323fb7dfca964362471c32
Princecoding2009 <fathi.ghannam@jpschools.org>
2026-03-24 15:20:06 +0000

    Where did i left out

--- asd-projects/dice-app/dice.js
+++ asd-projects/dice-app/dice.js
@@ -1,11 +1,34 @@
 $(document).ready(function () {
   // Your code goes here
-$("<div>")
-    .css("height", 15)
-    .css("width", 15)
+  $("<div>")
+    .css("height", 75)
+    .css("width", 75)
     .css("background-color", "black")
     .css("position", "absolute")
-    .css("top", 50)
-    .css("left", 50)
+    .css("top", 212.5)
+    .css("left", 212.5)
     .appendTo("#die");
+
+  function makeDot(top, left, elementID) {
+    $("<div>")
+      .css("height", 75)
+      .css("width", 75)
+      .css("background-color", "black")
+      .css("position", "absolute")
+      .css("top", top)
+      .css("left", left)
+      .css("border-radius", "50%")
+      .appendTo(elementID);
+  }
+
+  function rollDie(dieId) {
+    $(dieId).empty();
+    console.log("clicked");
+    var randomNum = Math.ceil(Math.random() * 6);
+    console.log(randomNum);
+  }
+  function handleClick() {
+    rollDie("#die");
+  }
+  $("#die").on("click", handleClick);
 });
